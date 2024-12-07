11d0e69d56714da2790a677f5e6e8e10c718201c
gazi04 <gazmendhalili2016@gmail.com>
2024-12-07 22:15:24 +0200

    Renamed the technologist middleware and edit the middleware in the routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\DoctorMiddleware;
 use App\Http\Middleware\PatientMiddleware;
 use App\Http\Middleware\NurseMiddleware;
-use App\Http\Middleware\TechnologitsMiddleware;
+use App\Http\Middleware\TechnologistMiddleware;
 use App\Http\Middleware\ReceptionistMiddleware;
 
 Route::get('/', function () {
@@ -17,9 +18,27 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::get('/admin-dashboard',function(){ return 'admin dashboar';})->name('admin-dashboard')->middleware(AdminMiddleware::class);
-Route::get('/doctor-dashboard',function(){ return 'doctor dashboar';})->name('doctor-dashboard')->middleware(DoctorMiddleware::class);
-Route::get('/patient-dashboard',function(){ return 'patient dashboar';})->name('patient-dashboard')->middleware(PatientMiddleware::class);
-Route::get('/nurse-dashboard',function(){ return 'nurse dashboar';})->name('nurse-dashboard')->middleware(NurseMiddleware::class);
-Route::get('/technologist-dashboard',function(){ return 'techonolgists dashboar';})->name('technologist-dashboard')->middleware(TechnologitsMiddleware::class);
-Route::get('/receptionist-dashboard',function(){ return 'receptionist dashboar';})->name('receptionist-dashboard')->middleware(ReceptionistMiddleware::class);
+/* Route::get('/admin-dashboard', [AdminController::class, 'dashboard'])->name('admin-dashboard')->middleware(AdminMiddleware::class); */
+Route::middleware(AdminMiddleware::class)->group(function () {
+    Route::get('/admin-dashboard', [AdminController::class, 'dashboard'])->name('admin-dashboard');
+});
+
+Route::middleware(DoctorMiddleware::class)->group(function () {
+    Route::get('/doctor-dashboard',function(){ return 'doctor dashboar';})->name('doctor-dashboard');
+});
+
+Route::middleware(PatientMiddleware::class)->group(function () {
+    Route::get('/patient-dashboard',function(){ return 'patient dashboar';})->name('patient-dashboard');
+});
+
+Route::middleware(NurseMiddleware::class)->group(function () {
+    Route::get('/nurse-dashboard',function(){ return 'nurse dashboar';})->name('nurse-dashboard');
+});
+
+Route::middleware(TechnologistMiddleware::class)->group(function () {
+    Route::get('/technologist-dashboard',function(){ return 'techonolgists dashboar';})->name('technologist-dashboard');
+});
+
+Route::middleware(ReceptionistMiddleware::class)->group(function () {
+    Route::get('/receptionist-dashboard',function(){ return 'receptionist dashboar';})->name('receptionist-dashboard');
+});
