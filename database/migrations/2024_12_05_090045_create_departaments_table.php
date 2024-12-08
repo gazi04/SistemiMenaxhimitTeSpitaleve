c8e304d81aef1d8b2d6839796602a2a54d7de8a1
gazi04 <gazmendhalili2016@gmail.com>
2024-12-08 17:21:57 +0200

    Added the unique property to the name column.

--- database/migrations/2024_12_05_090045_create_departaments_table.php
+++ database/migrations/2024_12_05_090045_create_departaments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('departaments', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name', 100)->unique();
             $table->string('description');
             $table->timestamps();
         });
