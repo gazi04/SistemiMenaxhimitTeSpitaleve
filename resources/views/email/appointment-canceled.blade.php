cb4738e032822b420c73cbd367c90a18dd11f68f
gazi04 <gazmendhalili2016@gmail.com>
2025-01-14 21:27:06 +0200

    Adding a template to the emails.

--- resources/views/email/appointment-canceled.blade.php
+++ resources/views/email/appointment-canceled.blade.php
@@ -1,14 +1,51 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Njoftimi për Anulimi të Takimit</title>
-</head>
-<body>
-    <h1>Pershendetje {{ $patient_name }},</h1>
-    <p>Ky email është për t'ju informuar për një anulim të takimit tuaj më {{ \Carbon\Carbon::parse($appointment_date)->format('d-m-Y H:i') }}.</p>
-    <p><b>Me respekt,</b></p>
-    <p>{{ $doctor_name }}.</p>
-    <p>Ju lutemi mos iu përgjigjni këtij emaili sepse ky është një email i automatizuar.</p>
-</body>
+    <head>
+        <title>Njoftimi për anulim të takimit</title>
+        <style>
+        body {
+            font-family: Rubik, sans-serif;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        .email-container {
+            background-color: #fff;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            text-align: center;
+            border-left: 7px solid blue;
+        }
+        h1 {
+            font-size: 24px;
+            color: #444;
+        }
+        p {
+            font-size: 16px;
+            line-height: 1.6;
+            margin: 10px 0;
+        }
+        p b {
+            color: #555;
+        }
+        .footer {
+            font-size: 14px;
+            color: #999;
+            margin-top: 20px;
+        }
+        </style>
+    </head>
+    <body>
+        <div class="email-container">
+            <h1>Përshendetje {{ $patient_name }},</h1>
+            <p>Ky email është për t'ju informuar për një anulim të takimit tuaj më {{ \Carbon\Carbon::parse($appointment_date)->format('d-m-Y H:i') }}.</p>
+            <p><b>Me respekt,</b></p>
+            <p>{{ $doctor_name }}.</p>
+            <p class="footer">Ju lutemi të mos iu përgjigjeni këtij emaili, sepse ky është një email i autorizuar.</p>
+        </div>
+    </body>
 </html>
-
--- resources/views/email/send-patient-id.blade.php
+++ resources/views/email/send-patient-id.blade.php
@@ -1,13 +1,51 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ID jote e pacientit</title>
+    <title>ID e pacientit</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            color: #333;
+            margin: 0;
+            padding: 0;
+        }
+        .email-container {
+            background-color: #fff;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
+            margin: 20px auto;
+            padding: 20px;
+            text-align: center;
+            border-left: 7px solid blue;
+        }
+        h1 {
+            font-size: 24px;
+            color: #444;
+        }
+        p {
+            font-size: 16px;
+            line-height: 1.6;
+            margin: 10px 0;
+        }
+        p b {
+            color: #555;
+        }
+        .footer {
+            font-size: 14px;
+            color: #999;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h1>Pershendetje {{ $firstName }},</h1>
-    <p>Kjo eshte ID-ja jote per llogarine e pacientit eshte: <strong>{{ $patientId }}</strong></p>
-    <p>Kshtu qe ruaj ate ne nje vend te sigurt.</p>
-    <br>
-    <p>Ju lutemi mos iu përgjigjni këtij emaili sepse ky është një email i automatizuar.</p>
+    <div class="email-container">
+        <h1>Pershendetje {{ $firstName }},</h1>
+        <p>Kjo është ID-ja yte për llogarinë e pacientit: <strong>{{ $patientId }}</strong></p>
+        <p>Kështu që ruaje atë në një vend të sigurtë.</p>
+        <br>
+        <p class="footer">Ju lutemi të mos iu përgjigjeni këtij emaili, sepse ky është një email i autorizuar.</p>
+    </div>
 </body>
 </html>
